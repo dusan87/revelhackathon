9eac12ea37223831b5a5606d3a90e2856c36e9d7
Dusan Ristic <dusan.ristic@revelsystems.com>
2016-09-05 16:25:55 +1000

    Using click to perform app related commands

--- run.py
+++ run.py
@@ -1,6 +1,23 @@
 #!flask/bin/python
+import click
+
 from app import app
 from app.database import create_all
 
-create_all()
-app.run()
+
+@click.group()
+def execute():
+    pass
+
+
+@execute.command(help='Setup database schema')
+def initdb():
+    create_all()
+
+
+@execute.command(help='Run application')
+def application():
+    app.run(debug=True)
+
+if __name__ == '__main__':
+    execute()
